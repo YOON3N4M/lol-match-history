1eeb832833d705c364cd7d045ba35b95deb567c9
yoon3nam <lgmysn2@gmail.com>
2025-01-13 10:06:09 +0900

    chore: images remotepatterns 추가

--- next.config.js
+++ next.config.js
@@ -1,28 +1,33 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-	reactStrictMode: false,
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'http',
-				hostname: 'ddragon.leagueoflegends.com',
-				port: '',
-			},
-		],
-	},
-	env: {
-		API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-		AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-		PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
-		STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-		MESSAGING_ID: process.env.NEXT_PUBLIC_MESSAGING_ID,
-		APP_ID: process.env.NEXT_PUBLIC_APP_ID,
-		RIOT_API_KEY: process.env.NEXT_PUBLIC_RIOT_API_KEY,
-	},
-	compiler: {
-		styledComponents: true,
-	},
-}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+        pathname: "/cdn/**",
+      },
+      {
+        protocol: "http",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+        pathname: "/cdn/**",
+      },
+    ],
+  },
+  env: {
+    API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+    AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+    STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    MESSAGING_ID: process.env.NEXT_PUBLIC_MESSAGING_ID,
+    APP_ID: process.env.NEXT_PUBLIC_APP_ID,
+    RIOT_API_KEY: process.env.NEXT_PUBLIC_RIOT_API_KEY,
+  },
+  compiler: {
+    styledComponents: true,
+  },
+};
