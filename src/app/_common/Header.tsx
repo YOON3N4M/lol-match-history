c6c324cb7d446ca1e951bc4d118eedffc489b3d0
yoon3n4m <lgmysn2@gmail.com>
2026-05-28 09:17:26 +0900

    modify: header content-layout

--- src/app/_common/Header.tsx
+++ src/app/_common/Header.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="min-h-[50px] bg-main-500">
-      <div className="flex justify-between px-6 py-2">
-        <h1 className="font-extrabold! text-2xl! text-white">
+      <div className="content-layout flex justify-between py-2">
+        <h1 className="text-2xl! font-extrabold! text-white">
           <Link href="/">33.GG</Link>
         </h1>
       </div>
