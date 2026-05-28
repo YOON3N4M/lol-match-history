ab1d7e6cdc500f6b84e413c2b06a6430ae2c1654
yoon3n4m <lgmysn2@gmail.com>
2026-05-28 13:38:26 +0900

    rename: 이미지 이동

--- src/constants/riot/position-icon.ts
+++ src/constants/riot/position-icon.ts
@@ -1,19 +1,16 @@
 import type { ParticipantDto } from "@/types/riot";
 
-import TOP_ICON from "@/img/lane/top.svg";
-import JG_ICON from "@/img/lane/jg.svg";
-import MID_ICON from "@/img/lane/mid.svg";
-import ADC_ICON from "@/img/lane/adc.svg";
-import SUP_ICON from "@/img/lane/sup.svg";
-
-export type LanePosition = Exclude<ParticipantDto["individualPosition"], "Invalid">;
+export type LanePosition = Exclude<
+  ParticipantDto["individualPosition"],
+  "Invalid"
+>;
 
 export const POSITION_ICON_BY_POSITION = {
-  TOP: TOP_ICON.src,
-  JUNGLE: JG_ICON.src,
-  MIDDLE: MID_ICON.src,
-  BOTTOM: ADC_ICON.src,
-  UTILITY: SUP_ICON.src,
+  TOP: "/images/lane/top.svg",
+  JUNGLE: "/images/lane/jg.svg",
+  MIDDLE: "/images/lane/mid.svg",
+  BOTTOM: "/images/lane/adc.svg",
+  UTILITY: "/images/lane/sup.svg",
 } satisfies Record<LanePosition, string>;
 
 export const POSITION_ICON_LIST = [
@@ -21,5 +18,9 @@ export const POSITION_ICON_LIST = [
   { position: "JUNGLE", icon: POSITION_ICON_BY_POSITION.JUNGLE, alt: "jungle" },
   { position: "MIDDLE", icon: POSITION_ICON_BY_POSITION.MIDDLE, alt: "mid" },
   { position: "BOTTOM", icon: POSITION_ICON_BY_POSITION.BOTTOM, alt: "bottom" },
-  { position: "UTILITY", icon: POSITION_ICON_BY_POSITION.UTILITY, alt: "support" },
+  {
+    position: "UTILITY",
+    icon: POSITION_ICON_BY_POSITION.UTILITY,
+    alt: "support",
+  },
 ] as const;
